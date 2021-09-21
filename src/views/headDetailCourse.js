078f42d488ad9731d4542ba8c38c7a8b69c9aec1
Mauri548 <mauricioferreyra548@gmail.com>
2021-09-21 00:23:19 -0300

    feature/ navigation to course detail

--- src/views/headDetailCourse.js
+++ src/views/headDetailCourse.js
@@ -1,11 +1,10 @@
-function headDetailCourse() {
+function headDetailCourse(course) {
     return `
     <div class="inicio">
         <div class="conteiner-welcome">
-            <h1 class="title">Getting Started with JavaScript</h1>
+            <h1 class="title">Getting Started with ${course.lenguaje}</h1>
             <p class="description">
-                JavaScript is the popular programming lenguage which powers web pages and web applications. 
-                This course will get you started coding in JavaScript.
+                ${course.description}
             </p>
             <div class="description2">
                 <div class="bookmark">
@@ -17,7 +16,7 @@ function headDetailCourse() {
                 <div class="view d-flex align-items-center">
                     <span class="text-white">
                         <i class="icon-grey far fa-user"></i>
-                        1200
+                        ${course.views}
                     </span>
                     <p class="text-white my-1">Enrolled</p>
                 </div>
@@ -29,7 +28,7 @@ function headDetailCourse() {
                         <i class="fas fa-star"></i>
                         <i class="fas fa-star"></i>
                     </span>
-                    <p class="text-white my-1">(140)</p>
+                    <p class="text-white my-1">(${course.points})</p>
                 </div>
             </div>
         </div>
