25b4711bb66cf9548a4018174fff9ad5d12ee9a0
Mauri548 <mauricioferreyra548@gmail.com>
2021-09-26 20:24:58 -0300

    limpieza de js que no se usa

--- src/views/alertError.js
+++
@@ -1,7 +0,0 @@
-function msgError() {
-    return `
-    <div class="conteiner-error-msg">
-        <span class="msg-error text-Light-purple">Error en la busqueda, vuelva a intentarlo mas tarde</span>
-    </div>
-    `
-}
