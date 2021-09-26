37c5a5c8869eed6fc55b817be043cda271d716e6
Mauri548 <mauricioferreyra548@gmail.com>
2021-09-25 22:47:07 -0300

    visibilidad de clases

--- src/object/User.ts
+++ src/object/User.ts
@@ -1,8 +1,32 @@
 class User {
-    _id!: number
-    _username!: string
-    _password!: string
-    _favourite!: []
-    _courses!: []
-    _state!: boolean
+    private _id!: number
+    private _username!: string
+    private _password!: string
+    private _favourite!: []
+    private _courses!: []
+    private _state!: boolean
+
+    get id(): number {
+        return this._id
+    }
+
+    get username(): string {
+        return this._username
+    }
+
+    get password(): string {
+        return this._password
+    }
+
+    get favourite(): [] {
+        return this._favourite
+    }
+
+    get course(): [] {
+        return this._courses
+    }
+
+    get state(): boolean {
+        return this._state
+    }
 }
--- src/object/Video.ts
+++ src/object/Video.ts
@@ -1,5 +1,17 @@
 class Video {
-    _id!: number
-    _duration!: number
-    _video!: string
+    private _id!: number
+    private _duration!: number
+    private _video!: string
+
+    get id(): number {
+        return this._id
+    }
+
+    get duration(): number {
+        return this._duration
+    }
+
+    get video(): string {
+        return this._video
+    }
 }
