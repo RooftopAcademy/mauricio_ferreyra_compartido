60d3c48a8361098259c6820698ef8cbe3659828c
Mauri548 <mauricioferreyra548@gmail.com>
2021-09-30 19:00:46 -0300

    comment login

--- src/login.ts
+++ src/login.ts
@@ -1,18 +1,26 @@
-import { toNumber } from "./helpers"
 import { store } from "./object/App"
 
-function login() {
-    // store.fetchUsers()
-    // console.log(store.users.all())
 
+export default function login() {
+
+    /**
+     * initialice form field
+     */
     let menssageError = document.getElementById('msg-error')
     let formulario = document.getElementById('login')!
     let username: HTMLInputElement = document.querySelector('.js-username')!
     let password: HTMLInputElement = document.querySelector('.js-password')!
     
+    /**
+     * Add event to login form
+     */
     formulario.addEventListener('submit', function(e) {
         e.preventDefault()
         store.users.all().forEach(user => {
+
+            /**
+             * verify that the user exists and save the id in the local storage
+             */
             if (user.login(username.value, password.value) != undefined) {
                 localStorage.setItem('user_id', user.login(username.value, password.value)?.toString()!)
                 window.location.href = "/index.html"
@@ -22,4 +30,4 @@ function login() {
     })
 }
 
-export default login
+
