8f872ff56c01e21b83d4304a681989de0429ab0a
Mauri548 <mauricioferreyra548@gmail.com>
2021-10-07 20:12:24 -0300

    rendering of the video list in the video playback view

--- src/courseVideo.ts
+++ src/courseVideo.ts
@@ -1,7 +1,19 @@
+import { toNumber } from "./helpers";
+import renderListVideos from "./helpers/renderListVideos";
 import renderNavbarAndAddLogic from "./helpers/renderNavbar";
+import toggleDropdownListOnClick from "./helpers/toggleDropdownListOnClick";
+import { store } from "./object/App";
+
 
 export default function courseVideo() {
     renderNavbarAndAddLogic()
 
-    
+    let course = store.catalog.findById(toNumber(localStorage.getItem('id_course')!))!
+    console.log(course)
+
+    renderListVideos(course, document.querySelector('.js-conteiner-list-videos') as HTMLElement)
+
+    toggleDropdownListOnClick(document.querySelectorAll('.theme-conteiner-item'))
+
+    // console.log(localStorage.getItem('id_video'))   
 }
--- src/productDetail.ts
+++ src/productDetail.ts
@@ -1,13 +1,10 @@
 import { render, toNumber } from "./helpers"
+import renderListVideos from "./helpers/renderListVideos"
 import renderNavbarAndAddLogic from "./helpers/renderNavbar"
+import toggleDropdownListOnClick from "./helpers/toggleDropdownListOnClick"
 import { store } from "./object/App"
-import { Course } from "./object/Course"
-import Theme from "./object/Theme"
-import Video from "./object/Video"
 import bodyDetailCourse from "./views/bodyDetailCourse"
-import conteinerThemeVideo from "./views/conteinerThemeVideo"
 import headDetailCourse from "./views/headDetailCourse"
-import itemLinkVideo from "./views/itemLinkVideo"
 
 /**
  * it is executed when you are in the detail of a product
@@ -27,30 +24,11 @@ export default function productDetail () {
     render(document.querySelector('.js-head-detail-course') as HTMLElement, headDetailCourse(course))
     render(document.querySelector('.js-body-detail-course') as HTMLElement, bodyDetailCourse(course))
 
-    renderListVideos(course)
+    renderListVideos(course, document.querySelector('.js-conteiner-list-videos') as HTMLElement)
 
-    toggleDropdownListOnClick()
-    
+    toggleDropdownListOnClick(document.querySelectorAll('.theme-conteiner-item'))
+
     addEventClickLinkVideo()
-}
-
-/**
- * 
- * change the display state of course topics
- * drop dow list
- * 
- */
-function toggleDropdownListOnClick () {
-    let themes = document.querySelectorAll('.theme-conteiner-item')
-    themes.forEach(theme => {
-        theme.addEventListener('click', function(e) {
-            e.preventDefault()
-            themes.forEach(element => {
-                if (element != theme) { element.classList.remove('active') }
-            })
-            theme.classList.contains('active') ? theme.classList.remove('active') : theme.classList.add('active')
-        })
-    })
 }
 
 
@@ -65,26 +43,6 @@ function addEventClickLinkVideo() {
     links.forEach(link => {
         link.addEventListener('click', function(e) {
             localStorage.setItem('id_video', this.dataset.videoId)
-        })
-    })
-}
-
-/**
- * 
- * dynamically create the list of videos in the html
- * 
- * @param course is a object course
- */
-function renderListVideos(course: Course) {
-    let conteinerVideos = document.querySelector('.js-conteiner-list-videos') as HTMLElement
-    course.themes.forEach(theme => {
-        render(conteinerVideos, conteinerThemeVideo(theme))
-        theme.videoList.forEach(video => {
-            document.querySelectorAll('.js-list-video-by-theme').forEach(el => {
-                if (parseInt(el.getAttribute('data-theme-id')!) == theme.id) {
-                    render(el as HTMLElement,itemLinkVideo(video))
-                }
-            })
         })
     })
 }
